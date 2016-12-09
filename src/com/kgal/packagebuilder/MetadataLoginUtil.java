7c6016fd69f5fbeea9d392c3737fb5f0f02f57ed
Kim Galant <kim@galant.info>
2016-12-09 09:55:11 +0800

    Working version of PackageBuilder with filtering.

---
+++ src/com/kgal/packagebuilder/MetadataLoginUtil.java
@@ -0,0 +1,95 @@
+package com.kgal.packagebuilder;
+
+import com.sforce.soap.partner.PartnerConnection;
+import com.sforce.soap.partner.LoginResult;
+
+import java.util.Properties;
+
+import com.sforce.soap.metadata.MetadataConnection;
+import com.sforce.ws.ConnectionException;
+import com.sforce.ws.ConnectorConfig;
+
+public class MetadataLoginUtil {
+	
+	private static final String APIVERSION = "35.0"; 
+	
+	/*
+	 * Properties key names
+	 */
+
+	private static final String USERNAME_PROPSKEY = "username";
+	private static final String PASSWORD_PROPSKEY = "password";
+	private static final String URL_PROPSKEY = "serverurl";
+	private static final String APIVERSION_PROPSKEY = "apiversion";
+	
+    /*
+     * Creates a MetadataConnection based on url, credentials
+     */
+	
+    public static MetadataConnection mdLogin(String url, String user, String pwd) throws ConnectionException {
+        final LoginResult loginResult = loginToSalesforce(user, pwd, url);
+        return createMetadataConnection(loginResult);
+    }
+    
+    /*
+     * Creates a MetadataConnection based on a properties object
+     */
+    
+    public static MetadataConnection mdLogin(Properties props) throws ConnectionException {
+    	String username = props.getProperty(USERNAME_PROPSKEY);
+    	String password = props.getProperty(PASSWORD_PROPSKEY);
+    	String url = props.getProperty(URL_PROPSKEY);
+    	
+    	if (username != null && password != null && url != null) {
+    		
+    		// check if it's the full url (contains /services/Soap/u/API), add if necessary
+    		
+    		if (!(url.contains("/services/Soap/u/"))) {
+    			url += "/services/Soap/u/" + props.getProperty(APIVERSION_PROPSKEY, APIVERSION);
+    		}
+    		
+    		return mdLogin(url, username, password);
+    	} else {
+    		return null;
+    	}
+    }
+    
+    public static PartnerConnection soapLogin(String url, String user, String pwd) {
+        
+    	PartnerConnection conn = null;
+
+        try {
+           ConnectorConfig config = new ConnectorConfig();
+           config.setUsername(user);
+           config.setPassword(pwd);
+
+           System.out.println("AuthEndPoint: " + url);
+           config.setAuthEndpoint(url);
+
+           conn = new PartnerConnection(config);
+           
+        } catch (ConnectionException ce) {
+           ce.printStackTrace();
+        } 
+
+        return conn;
+     }
+
+    private static MetadataConnection createMetadataConnection(final LoginResult loginResult) throws ConnectionException {
+        final ConnectorConfig config = new ConnectorConfig();
+        config.setServiceEndpoint(loginResult.getMetadataServerUrl());
+        config.setSessionId(loginResult.getSessionId());
+        return new MetadataConnection(config);
+    }
+
+    private static LoginResult loginToSalesforce(
+            final String username,
+            final String password,
+            final String loginUrl) throws ConnectionException {
+        final ConnectorConfig config = new ConnectorConfig();
+        config.setAuthEndpoint(loginUrl);
+        config.setServiceEndpoint(loginUrl);
+        config.setManualLogin(true);
+        return (new PartnerConnection(config)).login(username, password);
+    }
+}
--- src/com/kgal/packagebuilder/PackageBuilder.java
+++ src/com/kgal/packagebuilder/PackageBuilder.java
@@ -1,24 +1,16 @@
 package com.kgal.packagebuilder;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.channels.WritableByteChannel;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.io.filefilter.WildcardFileFilter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import com.sforce.soap.metadata.FileProperties;
 import com.sforce.soap.metadata.MetadataConnection;
@@ -27,7 +19,7 @@ import com.sforce.soap.metadata.*;
 
 import com.salesforce.migrationtoolutils.Utils;
 
-public class MetadataFetch {
+public class PackageBuilder {
 
 	String authEndPoint = "";
 
@@ -45,11 +37,10 @@ public class MetadataFetch {
 	
 	private static final double API_VERSION = 33.0;
 	private static double myApiVersion;
-	private static final int MAX_NUM_POLL_REQUESTS = 20000;
 	private static final int MAX_ITEMS=5000;
-	private static final String UNZIP = "false";
-	private static String myUnzip;
 	private static int myMaxItems;
+	private static String skipItems;
+	private static ArrayList<Pattern> skipPatterns = new ArrayList<Pattern>();
 	private static HashMap<String, DescribeMetadataObject> describeMetadataObjectsMap;
 	
 	private static final String[] allMdTypes = new String[] { "AccountSettings", "ActionOverride", "ActivitiesSettings", "AddressSettings", "AnalyticSnapshot",
@@ -70,7 +61,7 @@ public class MetadataFetch {
 	
 	public static void main(String[] args) throws RemoteException, Exception {
 
-		MetadataFetch sample = new MetadataFetch();
+		PackageBuilder sample = new PackageBuilder();
 		
 		if (args.length < 2) {
 			System.out.println("Usage parameters: <org property file path> <fetch property path>");
@@ -111,16 +102,20 @@ public class MetadataFetch {
 		}
 		
 		myApiVersion = sourceProps.getProperty("apiversion") == null ? API_VERSION : Double.parseDouble(sourceProps.getProperty("apiversion"));
-		srcUrl = sourceProps.getProperty("serverurl") + urlBase + myApiVersion;
-		srcUser = sourceProps.getProperty("username");
-		srcPwd = sourceProps.getProperty("password");
+		srcUrl = sourceProps.getProperty("sf_url") + urlBase + myApiVersion;
+		srcUser = sourceProps.getProperty("sf_username");
+		srcPwd = sourceProps.getProperty("sf_password");
 		myMaxItems = fetchProps.getProperty("maxItems") == null ? MAX_ITEMS : Integer.parseInt(fetchProps.getProperty("maxItems"));
-		myUnzip = fetchProps.getProperty("unzip") == null ? UNZIP : fetchProps.getProperty("unzip");
+		skipItems = fetchProps.getProperty("skipItems");
+		
+		
 		this.targetDir = Utils.checkPathSlash(Utils.checkPathSlash(fetchProps.getProperty("targetdirectory")) + "retrieved");
 		
 		System.out.println("target directory: " + this.targetDir);
 		
 		Utils.checkDir(targetDir);
+		
+		HashMap<String,ArrayList<String>> inventory = new HashMap<String,ArrayList<String>>(); 
 		
 		// Make a login call to source
 		this.srcMetadataConnection = MetadataLoginUtil.mdLogin(srcUrl, srcUser, srcPwd);
@@ -144,18 +139,93 @@ public class MetadataFetch {
 			System.out.println("*********************************************");
 			System.out.println("Processing: " + mdType);
 			System.out.println("*********************************************");
-			fetchMetadata(mdType);
+			ArrayList<String> mdTypeItemList = fetchMetadata(mdType);
+			
+			Collections.sort(mdTypeItemList);
+			
+			inventory.put(mdType, mdTypeItemList);
 			
 			System.out.println("---------------------------------------------");
 			System.out.println("Finished processing: " + mdType);
 			System.out.println("---------------------------------------------");
 			
 		}
+		
+		generatePackageXML(inventory);
 	}
 
-	private HashMap<String, ArrayList<String>> fetchMetadata (String metadataType) throws RemoteException, Exception {
+	private void generatePackageXML(HashMap<String, ArrayList<String>> inventory) {
+		StringBuffer packageXML = new StringBuffer();
+		packageXML.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+		packageXML.append("<Package xmlns=\"http://soap.sforce.com/2006/04/metadata\">\n");
+		
+		ArrayList<String> types = new ArrayList<String>();
+		types.addAll(inventory.keySet());
+		Collections.sort(types);
+		
+//		Initiate patterns array
+		
+		for (String p : skipItems.split(",")) {
+			try {
+				skipPatterns.add(Pattern.compile(p));
+			} catch (PatternSyntaxException  e) {
+				System.out.println("Tried to compile pattern: " + p + " but got exception: ");
+				e.printStackTrace();
+			}
+		}
+		boolean shouldSkip = false;
+		
+		for (String mdType : types) {
+			shouldSkip = false;
+//			first, check if any of the patterns match the whole type
+			String mdTypeFullName = mdType + ":";
+			for (Pattern p : skipPatterns) {
+				
+				Matcher m = p.matcher(mdTypeFullName);
+				if (m.matches()) {
+					System.out.println("Skip pattern: " + p.pattern() + " matches the metadata type: " + mdTypeFullName + ", entire type will be skipped.");
+					shouldSkip = true;
+					break;
+				}
+			}
+			if (shouldSkip) {
+				continue;
+			}
+			
+			ArrayList<String> items = inventory.get(mdType);
+			
+			packageXML.append("\t<types>\n");
+			for (String item : items) {
+				shouldSkip = false;
+				mdTypeFullName = mdType + ":" + item;
+				for (Pattern p : skipPatterns) {
+					Matcher m = p.matcher(mdTypeFullName);
+					if (m.matches()) {
+						System.out.println("Skip pattern: " + p.pattern() + " matches the metadata item: " + mdTypeFullName + ", item will be skipped.");
+						shouldSkip = true;
+						break;
+					}
+				}
+				if (!shouldSkip) {
+					packageXML.append("\t\t<members>" + item + "</members>\n");	
+				}
+					
+			}
+			
+			packageXML.append("\t\t<name>" + mdType + "</name>\n");
+			packageXML.append("\t</types>\n");
+		}
+		
+		packageXML.append("\t<version>" + myApiVersion + "</version>\n");
+		packageXML.append("</Package>\n");
+		
+		Utils.writeFile(targetDir + "package.xml", packageXML.toString());
+		
+	}
+
+	private ArrayList<String> fetchMetadata (String metadataType) throws RemoteException, Exception {
 		startTiming();
-		HashMap<String, ArrayList<String>> retval = new HashMap<String, ArrayList<String>>();
+		ArrayList<String> packageMap = new ArrayList<String>();
 		try {
 			
 			ArrayList<FileProperties> foldersToProcess = new ArrayList<FileProperties>();
@@ -188,7 +258,6 @@ public class MetadataFetch {
 			
 			HashMap<String, ArrayList<FileProperties>> metadataMap = new HashMap<String, ArrayList<FileProperties>>();
 			
-			int numfetches = 0;
 			int itemCount = 0;
 			
 			
@@ -212,6 +281,7 @@ public class MetadataFetch {
 				if (folderName != null) {
 					System.out.printf("%-80.80s","Processing folder: " + folderName + " ");
 					// fetch folders themselves
+					packageMap.add(folderName);
 					ArrayList<FileProperties> filenameList = new ArrayList<FileProperties>();
 					filenameList.add(folderProperties);
 					metadataMap.put(folderProperties.getFileName(), filenameList);
@@ -223,81 +293,26 @@ public class MetadataFetch {
 				
 				if (srcMd != null && srcMd.length > 0) {
 					for (FileProperties n : srcMd) {
-						ArrayList<FileProperties> filenameList = metadataMap.get(n.getFileName()); 
-						if (filenameList == null) {
-							filenameList = new ArrayList<FileProperties>();
-							metadataMap.put(n.getFileName(), filenameList);
-						} 
-						filenameList.add(n);
+						if (n.getNamespacePrefix() == null) {
+							packageMap.add(n.getFullName());	
+						}
+						
 					}
 				} else {
 					if (!isFolder) {
 						System.out.println("No items of this type, skipping...");
 						endTiming();
-						return retval;
+						return packageMap;
 					}
 					if (!folder.hasNext()) {
 						endTiming();
-						return retval;
+						return packageMap;
 					}
 				}
 				if (isFolder == true && folder.hasNext()) {
 					continue;
 				}
 
-				// now start pulling things off the metadataMap and fetching in chunks
-				
-				ArrayList<FileProperties> srcMdList = new ArrayList<FileProperties> ();
-				
-				for (ArrayList<FileProperties> filenameList : metadataMap.values()) {
-					// check if adding this list to the srcMdList would take it over the limit
-					// if there are files that are over the limit by themselves, they'll be fetched over limit anyway
-					
-					if (srcMdList.size() + filenameList.size() > myMaxItems) {
-						// yes, adding this would take current list over limit
-						// so fetch current list
-						
-						packageAndFetch(srcMdList, metadataType + "_" + numfetches++, metadataType);
-						
-						// clear current list
-						srcMdList.clear();
-					}	
-					// add the new item to the current list
-					srcMdList.addAll(filenameList);
-				}
-				
-				// last fetch 
-				
-				if (srcMdList.size() > 0) {
-					packageAndFetch(srcMdList, metadataType + "_" + numfetches++, metadataType);
-				}
-				
-				
-//				if (overFlow.size() == 0 && isFolder == true && folder.hasNext()) {
-//					// grab another folder
-//					continue;
-//				} else {
-//					packageAndFetch(srcMdList, metadataType + "_" + numfetches++, metadataType);
-//				}
-//				
-//				// now, if we have anything in overflow, remove it maxItems at a time and fetch
-//				
-//				srcMdList.clear();
-//				
-//				while (overFlow.size() > 0) {
-//					srcMdList.add(overFlow.remove(0));
-//					if (srcMdList.size() % myMaxItems == 0) {
-//						packageAndFetch(srcMdList, metadataType + "_" + numfetches++, metadataType);
-//						srcMdList.clear();
-//					}
-//				} 
-//				
-//				if (isFolder == true && folder.hasNext()) {
-//					continue;
-//				} else {
-//					packageAndFetch(srcMdList, metadataType + "_" + numfetches++, metadataType);
-//				}
-				
 			} while (folder.hasNext());
             
 		} catch (ConnectionException ce) {
@@ -306,137 +321,8 @@ public class MetadataFetch {
 			System.out.println(ce.getMessage());
 		}
 		
-		if (myUnzip.equals("true")) {
-			unzipAll(metadataType);
-		}
-		
 		endTiming();
-		return retval;
-	}
-
-	private void packageAndFetch(ArrayList<FileProperties> srcMdMap, String filename, String metadataType) throws Exception {
-		// create a package
-		
-		System.out.println("Asked to fetch: " + srcMdMap.size() + " items.");
-		
-		com.sforce.soap.metadata.Package r = new com.sforce.soap.metadata.Package();
-		
-		ArrayList<PackageTypeMembers> pd = new ArrayList<PackageTypeMembers>();
-		ArrayList<String> names = new ArrayList<String>();
-		
-		
-		PackageTypeMembers pdi = new PackageTypeMembers();
-		for (FileProperties n : srcMdMap) {				
-            names.add(n.getFullName());
-            
-			// stupid hack for emailtemplate folder name
-			String type = "";
-			if (metadataType.toLowerCase().equals("emailfolder")) {
-				type = "EmailTemplate";
-			} else if (metadataType.equals("DocumentFolder") ||
-					metadataType.equals("ReportFolder") ||
-					metadataType.equals("DashboardFolder")){
-				type = metadataType.substring(0, metadataType.indexOf("Folder"));
-			}
-			
-            pdi.setName(type == "" ? n.getType() : type);
-		}
-		
-		if (pdi != null && pdi.getName() != null && pdi.getName().equals("Flow")) {
-			names.clear();
-			names.add("*");
-		}
-		
-        pdi.setMembers(names.toArray(new String[names.size()]));
-        pd.add(pdi);
-        
-        r.setTypes(pd.toArray(new PackageTypeMembers[pd.size()]));
-        r.setVersion(API_VERSION + "");
-		
-		// create request
-        
-        RetrieveRequest retrieveRequest = new RetrieveRequest(); 
-        retrieveRequest.setApiVersion(myApiVersion);
-        retrieveRequest.setUnpackaged(r);
-		
-     // Start the retrieve operation
-        AsyncResult asyncResult = srcMetadataConnection.retrieve(retrieveRequest);
-        String asyncResultId = asyncResult.getId();
-        
-        // Wait for the retrieve to complete
-        int poll = 0;
-        long waitTimeMilliSecs = 10000;
-        RetrieveResult result = null;
-        do {
-            Thread.sleep(waitTimeMilliSecs);
-            
-            if (poll++ > MAX_NUM_POLL_REQUESTS) {
-                throw new Exception("Request timed out.  If this is a large set " +
-                "of metadata components, check that the time allowed " +
-                "by MAX_NUM_POLL_REQUESTS is sufficient.");
-            }
-            result = srcMetadataConnection.checkRetrieveStatus(
-                    asyncResultId, false);
-            System.out.println("Retrieve Status: " + result.getStatus());
-        } while (!result.isDone());
-
-        if (result.getStatus() == RetrieveStatus.Failed) {
-            throw new Exception(result.getErrorStatusCode() + " msg: " +
-                    result.getErrorMessage());
-        } else if (result.getStatus() == RetrieveStatus.Succeeded) {      
-            // Print out any warning messages
-            StringBuilder buf = new StringBuilder();
-            if (result.getMessages() != null) {
-                for (RetrieveMessage rm : result.getMessages()) {
-                    buf.append(rm.getFileName() + " - " + rm.getProblem());
-                }
-            }
-            if (buf.length() > 0) {
-                System.out.println("Retrieve warnings:\n" + buf);
-            }
-    
-            // Write the zip to the file system
-            System.out.println("Writing results to zip file");
-            ByteArrayInputStream bais = new ByteArrayInputStream(result.getZipFile());
-            File resultsFile = new File(targetDir + filename +  ".zip");
-            FileOutputStream os = new FileOutputStream(resultsFile);
-            try {
-                ReadableByteChannel src = Channels.newChannel(bais);
-                FileChannel dest = os.getChannel();
-                copy(src, dest);
-                
-                System.out.println("Results written to " + resultsFile.getAbsolutePath());
-            } finally {
-                os.close();
-            }
-            
-            
-        }
-	}
-	
-	private void unzipAll(String newDirLocation) {
-		FileFilter fileFilter = new WildcardFileFilter("*.zip");
-		File[] files = new File(targetDir).listFiles(fileFilter);
-		for (File f : files) {
-			Utils.unzip(f.getAbsolutePath(), targetDir);
-            String newFileName = Utils.checkPathSlash(targetDir) + newDirLocation;
-            Utils.checkDir(newFileName);
-            Utils.mergeTwoDirectories(new File(newFileName), new File(targetDir + "unpackaged"));
-			f.renameTo(new File(Utils.checkPathSlash(newFileName) + f.getName()));
-		}
-	}
-	
-	
-	private void copy(ReadableByteChannel src, WritableByteChannel dest) throws IOException {
-		// Use an in-memory byte buffer
-		ByteBuffer buffer = ByteBuffer.allocate(8092);
-		while (src.read(buffer) != -1) {
-			buffer.flip();
-			while (buffer.hasRemaining()) {
-				dest.write(buffer);
-			}
-			buffer.clear();
-		}
+		return packageMap;
 	}
 	
 	private void startTiming() {
