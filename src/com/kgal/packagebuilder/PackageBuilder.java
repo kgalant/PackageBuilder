6957746653086671eff605fe6d3a011d5d7d0225
Kim Galant <kim@galant.info>
2016-12-20 12:26:30 +0800

    Moved to proper command line handling

--- src/com/kgal/packagebuilder/PackageBuilder.java
+++ src/com/kgal/packagebuilder/PackageBuilder.java
@@ -17,6 +17,15 @@ import com.sforce.soap.metadata.MetadataConnection;
 import com.sforce.ws.ConnectionException;
 import com.sforce.soap.metadata.*;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionGroup;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 import com.salesforce.migrationtoolutils.Utils;
 
 public class PackageBuilder {
@@ -35,7 +44,7 @@ public class PackageBuilder {
 	private static final String urlBase = "/services/Soap/u/";
 	private String targetDir = "";
 	
-	private static final double API_VERSION = 33.0;
+	private static final double API_VERSION = 38.0;
 	private static double myApiVersion;
 	private static final int MAX_ITEMS=5000;
 	private static int myMaxItems;
@@ -44,41 +53,39 @@ public class PackageBuilder {
 	private static HashMap<String, DescribeMetadataObject> describeMetadataObjectsMap;
 	
 	private static HashSet<String> existingTypes = new HashSet<String>();
+	private static HashMap<String,String> parameters = new HashMap<String,String>();
 	
-	private static final String[] allMdTypes = new String[] { "AccountSettings", "ActionOverride", "ActivitiesSettings", "AddressSettings", "AnalyticSnapshot",
-			"ApexClass", "ApexComponent", "ApexPage", "ApexTrigger", "AppMenu", "ApprovalProcess", "ArticleType", "AssignmentRules", "AuthProvider",
-			"AutoResponseRules", "BaseSharingRule", "BusinessHoursSettings", "BusinessProcess", "CallCenter", "CaseSettings", "ChatterAnswersSettings",
-			"CompanySettings", "Community", "CompactLayout", "ConnectedApp", "ContractSettings", "CriteriaBasedSharingRule", "CustomApplication",
-			"CustomApplicationComponent", "CustomField", "CustomLabel", "CustomLabels", "CustomObject", "CustomObjectTranslation", "CustomPageWebLink",
-			"CustomPermission", "CustomSite", "CustomTab", "Dashboard", "DataCategoryGroup", "Document", "EmailTemplate", "EntitlementProcess",
-			"EntitlementSettings", "EntitlementTemplate", "ExternalDataSource", "FieldSet", "FlexiPage", "Flow", "Folder", "FolderShare",
-			"ForecastingSettings", "Group", "HomePageComponent", "HomePageLayout", "IdeasSettings", "InstalledPackage", "KnowledgeSettings", "Layout",
-			"Letterhead", "ListView", "LiveAgentSettings", "LiveChatAgentConfig", "LiveChatButton", "LiveChatDeployment", "Metadata", "MetadataWithContent",
-			"MilestoneType", "MobileSettings", "NamedFilter", "NameSettings", "Network", "OpportunitySettings", "OrderSettings", "OwnerSharingRule", "Package",
-			"PermissionSet", "Picklist", "Portal", "PostTemplate", "ProductSettings", "Profile", "Queue", "QuickAction", "QuoteSettings", "RecordType",
-			"RemoteSiteSetting", "Report", "ReportType", "Role", "SamlSsoConfig", "Scontrol", "SearchLayouts", "SecuritySettings", "SharingReason",
-			"SharingRecalculation", "SharingRules", "SharingSet", "SiteDotCom", "Skill", "StaticResource", "Territory", "SynonymDictionary", "Translations",
-			"ValidationRule", "Weblink", "Workflow","WorkflowAlert","WorkflowFieldUpdate","WorkflowKnowledgePublish","WorkflowOutboundMessage","WorkflowRule",
-			"WorkflowSend","WorkflowTask"};
+	private static CommandLine line = null;
+	private static Options options = new Options();
+	
+
 	
 	public static void main(String[] args) throws RemoteException, Exception {
 
 		PackageBuilder sample = new PackageBuilder();
+		setupOptions();
+		parseCommandLine(args);
 		
-		if (args.length < 2) {
-			System.out.println("Usage: java -jar PackageBuilder.jar <org property file path> <fetch property path>");
-			System.out.println("properties\test.properties properties\fetch.properties - will list the items defined in the fetch.properties file from the org specified in the file properties\test.properties and put them in the target directory specified in the properties\fetch.properties file ");
-			System.out.println("Parameters not supplied - exiting.");
-			System.exit(0);
-		}
 		
-		if (args.length > 0) {
-			sample.sourceProps = Utils.initProps(args[0]);
-		}
+
 		
-		if (args.length > 0) {
-			sample.fetchProps = Utils.initProps(args[1]);
-		}
+//		if (args.length < 2) {
+//			System.out.println("Usage: java -jar PackageBuilder.jar <org property file path> <fetch property path> [metadataitems=item1,item2,...] [skipitems=skipPattern1,skipPatter2,...]");
+//			System.out.println("properties\test.properties properties\fetch.properties - will list the items defined in the fetch.properties file from the org specified in the file properties\test.properties and put them in the target directory specified in the properties\fetch.properties file ");
+//			System.out.println("providing metadataitems and/or skipitems parameters on command line will override the same from the fetch properties file");
+//			System.out.println("Parameters not supplied - exiting.");
+//			System.exit(0);
+//		}
+//		
+//		if (args.length > 0) {
+//			sample.sourceProps = Utils.initProps(args[0]);
+//		}
+//		
+//		if (args.length > 1) {
+//			sample.fetchProps = Utils.initProps(args[1]);
+//		}
+//		
+		
 		
 		sample.run();
 	}
@@ -86,30 +93,26 @@ public class PackageBuilder {
 	public void run() throws RemoteException, Exception {
 		
 		HashSet<String> typesToFetch = new HashSet<String>();
-		HashSet<String> allMdTypesSet = new HashSet<String>();
 		
-		String mdTypesToExamine = fetchProps.getProperty("metadataitems") == null ? "" : fetchProps.getProperty("metadataitems");
+		String mdTypesToExamine = parameters.get("metadataitems");
 		
 		for (String s : mdTypesToExamine.split(",")) {
 			typesToFetch.add(s.trim());
 		}
 		
-		for (String s : allMdTypes) {
-			allMdTypesSet.add(s);
-			if (mdTypesToExamine == null || mdTypesToExamine.length() == 0) {
-				typesToFetch.add(s);
-			}
-		}
+		myApiVersion = Double.parseDouble(parameters.get("apiversion"));
+		srcUrl = parameters.get("sf_url") + urlBase + myApiVersion;
+		srcUser = parameters.get("sf_username");
+		srcPwd = parameters.get("sf_password");
+		skipItems = parameters.get("skipItems");
 		
-		myApiVersion = sourceProps.getProperty("apiversion") == null ? API_VERSION : Double.parseDouble(sourceProps.getProperty("apiversion"));
-		srcUrl = sourceProps.getProperty("sf_url") + urlBase + myApiVersion;
-		srcUser = sourceProps.getProperty("sf_username");
-		srcPwd = sourceProps.getProperty("sf_password");
-		myMaxItems = fetchProps.getProperty("maxItems") == null ? MAX_ITEMS : Integer.parseInt(fetchProps.getProperty("maxItems"));
-		skipItems = fetchProps.getProperty("skipItems") == null ? "" : fetchProps.getProperty("skipItems");
+		System.out.println("Will fetch: " + mdTypesToExamine);
+		System.out.println("From: " + srcUrl);
+		System.out.println("Using user: " + srcUser);
+		System.out.println("Skipping: " + skipItems);
 		
 		
-		this.targetDir = Utils.checkPathSlash(Utils.checkPathSlash(fetchProps.getProperty("targetdirectory")) + "retrieved");
+		this.targetDir = Utils.checkPathSlash(Utils.checkPathSlash(parameters.get("targetdirectory")));
 		
 		System.out.println("target directory: " + this.targetDir);
 		
@@ -137,9 +140,6 @@ public class PackageBuilder {
 		while (i.hasNext()) {
 			counter ++;
 			String mdType = i.next();
-			if (!allMdTypesSet.contains(mdType)) {
-				continue;
-			}
 			System.out.println("*********************************************");
 			System.out.println("Processing type " + counter + " out of " + workToDo.size() + ": " + mdType );
 			System.out.println("*********************************************");
@@ -358,5 +358,125 @@ public class PackageBuilder {
 		System.out.println("Duration: " + hms);
 	}
 	
+	private static void setupOptions() {
+
+		options.addOption( Option.builder("o").longOpt( "orgfile" )
+                .desc( "file containing org parameters (see below)" )
+                .hasArg()
+                .build() );
+		options.addOption( Option.builder("u").longOpt( "username" )
+		                                .desc( "username for the org (someuser@someorg.com)" )
+		                                .hasArg()
+		                                .build() );
+		options.addOption( Option.builder("p").longOpt( "password" )
+                .desc( "password for the org (t0pSecr3t)" )
+                .hasArg()
+                .build() );
+		options.addOption( Option.builder("s").longOpt( "serverurl" )
+                .desc( "server URL for the org (https://login.salesforce.com)" )
+                .hasArg()
+                .build() );
+		options.addOption( Option.builder("a").longOpt( "apiversion" )
+                .desc( "api version to use, will default to " + API_VERSION)
+                .hasArg()
+                .build() );
+		options.addOption( Option.builder("mi").longOpt( "metadataitems" )
+                .desc( "metadata items to fetch" )
+                .hasArg()
+                .build() );
+		options.addOption( Option.builder("sp").longOpt( "skippatterns" )
+                .desc( "patterns to skip when fetching" )
+                .hasArg()
+                .build() );
+		options.addOption( Option.builder("d").longOpt( "destination" )
+                .desc( "directory where the generated package.xml will be written" )
+                .hasArg()
+                .build() );
+	}
 	
+	private static void printHelp() {
+		HelpFormatter formatter = new HelpFormatter();
+	    formatter.setOptionComparator(null);
+
+	    formatter.printHelp( "java -jar PackageBuilder.jar [-o <parameter file1>,<parameter file2>] [-u <SF username>] [-p <SF password>]", options );
+	}
+	
+	private static void parseCommandLine(String[] args) {
+		
+		parameters.put("apiversion", "" + API_VERSION);
+		parameters.put("metadataitems", null);
+		parameters.put("skipItems", null);
+		parameters.put("sf_url", null);
+		parameters.put("sf_username", null);
+		parameters.put("sf_password", null);
+		parameters.put("targetdirectory", null);
+		
+		HashSet<String> nonMandatoryParams = new HashSet<String>();
+		nonMandatoryParams.add("skipItems");
+		
+		CommandLineParser parser = new DefaultParser();
+	    try {
+	        // parse the command line arguments
+	        line = parser.parse( options, args );
+	    }
+	    catch( ParseException exp ) {
+	        // oops, something went wrong
+	        System.err.println( "Command line parsing failed.  Reason: " + exp.getMessage() );
+	        System.exit(-1);
+	    }
+	    
+	    if (line != null) {
+	    	// first initialize parameters from any parameter files provided
+	    	
+	    	if (line.hasOption("o") && line.getOptionValue("o") != null && line.getOptionValue("o").length() > 0) {
+	    		String paramFilesParameter = line.getOptionValue("o");
+	    		for (String paramFileName : paramFilesParameter.split(",")) {
+	    			Properties props = Utils.initProps(paramFileName.trim());
+	    			System.out.println("Loading parameters from file: " + paramFileName);
+	    			parameters.put("apiversion", props.getProperty("apiversion") == null ? parameters.get("apiversion") : "" + Double.parseDouble(props.getProperty("apiversion")));
+	    			parameters.put("metadataitems", props.getProperty("metadataitems") == null ? parameters.get("metadataitems") : props.getProperty("metadataitems"));
+	    			parameters.put("sf_url", props.getProperty("sf_url") == null ? parameters.get("sf_url") : props.getProperty("sf_url"));
+	    			parameters.put("sf_username", props.getProperty("sf_username") == null ? parameters.get("sf_username") : props.getProperty("sf_username"));
+	    			parameters.put("sf_password", props.getProperty("sf_password") == null ? parameters.get("sf_password") : props.getProperty("sf_password"));
+	    			parameters.put("skipItems", props.getProperty("skipItems") == null ? parameters.get("skipItems") : props.getProperty("skipItems"));
+	    			parameters.put("targetdirectory", props.getProperty("targetdirectory") == null ? parameters.get("targetdirectory") : props.getProperty("targetdirectory"));
+	    		}
+	    	}
+	    	
+	    	// now add all parameters form the commandline
+	    	if (line.hasOption("a") && line.getOptionValue("a") != null && line.getOptionValue("a").length() > 0) {
+	    		parameters.put("apiversion", line.getOptionValue("a"));
+	    	}
+	    	if (line.hasOption("u") && line.getOptionValue("u") != null && line.getOptionValue("u").length() > 0) {
+	    		parameters.put("sf_username", line.getOptionValue("u"));
+	    	}
+	    	if (line.hasOption("s") && line.getOptionValue("s") != null && line.getOptionValue("s").length() > 0) {
+	    		parameters.put("sf_url", line.getOptionValue("s"));
+	    	}
+	    	if (line.hasOption("p") && line.getOptionValue("p") != null && line.getOptionValue("p").length() > 0) {
+	    		parameters.put("sf_password", line.getOptionValue("u"));
+	    	}
+	    	if (line.hasOption("mi") && line.getOptionValue("mi") != null && line.getOptionValue("mi").length() > 0) {
+	    		parameters.put("metadataitems", line.getOptionValue("mi"));
+	    	}
+	    	if (line.hasOption("sp") && line.getOptionValue("sp") != null && line.getOptionValue("sp").length() > 0) {
+	    		parameters.put("skipItems", line.getOptionValue("sp"));
+	    	}
+	    	if (line.hasOption("d") && line.getOptionValue("d") != null && line.getOptionValue("d").length() > 0) {
+	    		parameters.put("targetdirectory", line.getOptionValue("d"));
+	    	}
+	    	// check that we have the minimum parameters 
+	    	boolean canProceed = true;
+	    	for (String key : parameters.keySet()) {
+	    		if (!nonMandatoryParams.contains(key) && (parameters.get(key) == null || parameters.get(key).length() < 1)) {
+	    			System.out.println("Parameter " + key + " not provided in properties files or on command line, cannot proceed.");
+	    			canProceed = false;
+	    		}
+	    	}
+	    	if (!canProceed) {
+	    		printHelp();
+	    		System.exit(1);
+	    	}
+    	} else printHelp();
+	}
 }
