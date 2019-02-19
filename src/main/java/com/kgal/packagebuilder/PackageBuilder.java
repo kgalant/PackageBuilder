639dd35e0411ab4bae30abcb6323b5997845fe07
Kim Galant <kim@galant.info>
2019-02-19 17:15:43 +0800

    Removed SimpleXMLDoc wrapper from project, replaced with DOMBuilder

--- src/main/java/com/kgal/packagebuilder/PackageBuilder.java
+++ src/main/java/com/kgal/packagebuilder/PackageBuilder.java
@@ -36,7 +36,6 @@ import org.w3c.dom.Element;
 
 import com.kgal.packagebuilder.inventory.InventoryItem;
 import com.kgal.packagebuilder.output.GitOutputManager;
-import com.kgal.packagebuilder.output.SimpleXMLDoc;
 import com.salesforce.migrationtoolutils.Utils;
 import com.sforce.soap.metadata.DescribeMetadataObject;
 import com.sforce.soap.metadata.DescribeMetadataResult;
